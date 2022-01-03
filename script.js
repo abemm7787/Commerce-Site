b901cee57cfb0c9820a307ec4fd8cc2466081c55
unknown <mirambeaux7@gmail.com>
2022-01-03 12:06:30 -0600

    the  our story paragraph has been  move  and a object has been implemented next to our boots

--- script.js
+++ script.js
@@ -3,16 +3,18 @@
 let boots = {
     model: "Black Panther",
     brand: "Boots",
+    img: "./images/boots2.jpg",
     description: "2020 style makes feet look thinner",
-    myOrder(){
+    myOrder() {
         return ` You order The New ${this.model} from ${this.brand}.com`
 
     }
-    
+
 
 }
 
 
 console.log(boots.model)
-
 console.log(boots.myOrder())
+
+$(".product-image").attr("src", boots.img)
