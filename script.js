da9a79bf472ddf7dd875a48ae15563acf6f1500c
unknown <mirambeaux7@gmail.com>
2022-01-03 13:47:44 -0600

    added a constructor

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 
 
 let boots = {
+
+
     model: "Black Panther",
     brand: "Boots",
     img: "./images/boots2.jpg",
@@ -10,11 +12,65 @@ let boots = {
 
     }
 
+}
+
+
+
+let boots2 = {
+  model: "White Panther",
+    brand: "Boots",
+    img: "./images/boots2.jpg",
+    description: "2021 softer than feathers",
+    myOrder() {
+        return ` You order The New ${this.model} from ${this.brand}.com`
+
+    }
 
 }
 
 
-console.log(boots.model)
-console.log(boots.myOrder())
-
+let boots3 = {
+    model: " Lug Bug",
+      brand: "Boots",
+      img: "./images/boots2.jpg",
+      description: "Good to walk on winter snow",
+      myOrder() {
+          return ` You order The New ${this.model} from ${this.brand}.com`
+  
+      }
+  
+  }
+  
+// boot #1
 $(".product-image").attr("src", boots.img)
+$(".product-text").text(boots.description)  
+
+//boot #2
+$(".product-image").attr("src" , boots2.img)
+$(".product-text").text(boots2.description)
+
+
+//boot #3
+$(".product-image").attr("src", boots3.img)
+$("product-text").text(boots3.description)
+
+
+// Constructor 
+
+function Boots(){
+
+    console.log(" Boot Initialized bruh")
+
+}
+
+// instatiate an object
+ boots = new Boots();
+ boots2 = new Boots();
+ boot3 =  new Boots();
+
+
+console.log(boots.model)
+
+console.log(boots2)
+
+console.log(boots3)
